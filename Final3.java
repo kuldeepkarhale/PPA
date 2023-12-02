719ae139d1e7e3ddfcec9a9df6c562f133682da4
kuldeepkarhale <152726834+kuldeepkarhale@users.noreply.github.com>
2023-12-02 23:40:19 +0530

    Add files via upload

---
+++ Final3.java
@@ -0,0 +1,22 @@
+ class Demo
+{
+    public void fun()
+    {}
+    public final void gun()
+    {}
+
+}
+class Hello extends Demo
+{
+    public void fun()
+    {}
+    public void gun()
+    {}
+}
+class Final3
+{
+    public static void main(String arg[]);
+    {
+
+    }
+}
